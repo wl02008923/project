ab372692d29d6aee1ca24763917d9f5c9a3f27ad
unknown <>
2016-12-01 17:50:15 +0800

    12/1

---
+++ js/goodsJs.js
@@ -0,0 +1,14 @@
+    $(document).ready(function(){ 
+      $.datepicker.regional['zh-TW']={
+        dayNames:["星期日","星期一","星期二","星期三","星期四","星期五","星期六"],
+        dayNamesMin:["日","一","二","三","四","五","六"],
+        monthNames:["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"],
+        monthNamesShort:["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"],
+        prevText:"上月",
+        nextText:"次月",
+        weekHeader:"週"
+        };
+      $.datepicker.setDefaults($.datepicker.regional["zh-TW"]);
+      $("#datepicker1").datepicker({dateFormat:"yy-mm-dd",showMonthAfterYear:true});
+      $("#datepicker2").datepicker({dateFormat:"yy-mm-dd",showWeek:true});
+      });
